0883ff9f3b73f2be7d41a7d70404d57145274eeb
Nick Massa <massa.nicholas.x@dol.gov>
2020-05-11 19:58:00 -0400

    Adds Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import StateNav from './routing';
 import logo from './logo.svg';
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  ReactGA.initialize('UA-166233935-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <header className="App-header">
