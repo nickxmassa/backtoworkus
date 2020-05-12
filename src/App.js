3ff05fdd9c623e0cb8d4492b0d9b3bcd39752e6b
Nick Massa <massa.nicholas.x@dol.gov>
2020-05-11 20:21:36 -0400

    Adds HashRouter to maintain location on refresh

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga';
 import StateNav from './routing';
 import logo from './logo.svg';
+import { HashRouter } from 'react-router-dom'
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,11 +11,13 @@ function App() {
   ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
+    <HashRouter>
     <div className="App">
       <header className="App-header">
         <StateNav />
       </header>
     </div>
+    </HashRouter>
   );
 }
 
