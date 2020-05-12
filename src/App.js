fada21995641631981fe84b2af9279c7242a6b8b
Nick Massa <massa.nicholas.x@dol.gov>
2020-05-11 20:31:34 -0400

    Removes HashRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactGA from 'react-ga';
 import StateNav from './routing';
 import logo from './logo.svg';
-import { HashRouter } from 'react-router-dom'
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,13 +10,11 @@ function App() {
   ReactGA.pageview(window.location.pathname + window.location.search);
 
   return (
-    <HashRouter>
     <div className="App">
       <header className="App-header">
         <StateNav />
       </header>
     </div>
-    </HashRouter>
   );
 }
 
